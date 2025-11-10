8142883bbcb521a59439eca3a447c4b44071d5de
Onyshchenko Dmytro <60102637+onyshchenkodmytro@users.noreply.github.com>
2025-11-10 14:20:07 +0200

    Update Config.cs

--- EdoAuthServer/Config.cs
+++ EdoAuthServer/Config.cs
@@ -46,7 +46,7 @@ public static class Config
             RequireClientSecret = true,
             ClientSecrets = { new Secret("secret".Sha256()) },
 
-            // 🚨 Перевір порти (повинні збігатися з твоїм MVC-проєктом)
+            
             RedirectUris =           { "https://localhost:7275/signin-oidc" },
             PostLogoutRedirectUris = { "https://localhost:7275/signout-callback-oidc" },
             FrontChannelLogoutUri =  "https://localhost:7275/signout-oidc",
